22a16bd0e5c4da750bee699377ae0db5734f65b8
naeemehetesami <naeemeh.etesamii@gmail.com>
2021-01-06 14:22:03 +0000

    my second  commit

--- main_n.py
+++ main_n.py
@@ -1,10 +1,12 @@
 import firebase_access
 import TestofCamera
 import uploadtest
+# import save_from_camera
 
 if __name__ == '__main__':
     serialnumber = input('Please insert serial number: ')
     UserID = input('which user?: ')
     MyDrive = uploadtest.Authentication()
     TestofCamera.Camera(serialnumber,UserID)
+    # save_from_camera.SavePhotos(serialnumber,UserID)
     firebase_access.FirebaseAccess.Readforupload(serialnumber, UserID, MyDrive)
